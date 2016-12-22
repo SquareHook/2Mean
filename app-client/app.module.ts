43bac70f286800b5ca15387aa0f889253e187d09
Jason Bennett <jasonb@squarehook.com>
2016-12-22 12:19:58 -0700

    Added Angular 2 bootstrap dependency

--- app-client/app.module.ts
+++ app-client/app.module.ts
@@ -2,8 +2,13 @@ import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
 
+import { NgbModule }      from '@ng-bootstrap/ng-bootstrap';
+
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [
+    BrowserModule,
+    NgbModule.forRoot()
+  ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
