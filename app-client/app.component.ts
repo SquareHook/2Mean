76bce2f4e51537e7650f1dced3243dcbbb956432
Jason Bennett <jasonb@squarehook.com>
2016-12-22 16:04:47 -0700

    Resolved some issues loading the auth component
    Still having issues with the html for the new component

--- app-client/app.component.ts
+++ app-client/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'too-mean',
-  template: `<h1>Testing {{ name }}</h1>`
+  template: `
+    <h1>Testing {{ name }}</h1>
+    <login-form></login-form>
+  `
 })
 
 export class AppComponent {
--- app-client/app.module.ts
+++ app-client/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
+import { HttpModule }     from '@angular/http';
 
 import { NgbModule }      from '@ng-bootstrap/ng-bootstrap';
+
+import { LoginComponent } from './auth/auth.component.client';
 
 @NgModule({
   imports:      [
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    HttpModule
   ],
-  declarations: [ AppComponent ],
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
   bootstrap:    [ AppComponent ]
 })
 
