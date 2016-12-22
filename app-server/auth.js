0b6a233e885e06934a05b95c9d4413ca03711c30
Jason Bennett <jasonb@squarehook.com>
2016-12-22 14:07:17 -0700

    unsuccessful static route attempts

--- app-server/auth.js
+++ app-server/auth.js
@@ -1,6 +1,11 @@
-var jwt = require('jwt-simple');
-
 function auth() {
   function login(req, res) {
+    res.status(200).send({message: 'Nothing to see here'});
+  }
+
+  return {
+    login: login
   }
 }
+
+module.exports = new auth();
