3b16ca9ca860f74c1bb391ea38974573c8dc8c1b
Hayden Shelton <hayden.shelton@utah.edu>
2016-12-27 14:53:46 -0700

    groundwork for core module with menu

---
+++ app-client/core/core.component.menu.client.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector:'core-menu',
+	providers: [],
+	templateUrl: './core.component.client.html'
+})
+
+export class CoreMenuComponent{
+	constructor()
+	{
+		console.log('Core Menu Component Imported');
+	}
+}
+
