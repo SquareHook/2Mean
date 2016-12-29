c837bb272d74467ae4dc7e933730ae109b75ff24
Hayden Shelton <hayden.shelton@utah.edu>
2016-12-29 09:42:26 -0700

    basic menu working

--- app-client/core/components/core.component.menu.client.ts
+++ app-client/core/components/core.component.menu.client.ts
@@ -12,12 +12,12 @@ export class CoreMenuComponent{
 	menu_items : any
 	constructor()
 	{
-		//TODO: fix this awefulness
+		//do some formatting to get proper JSON
 		this.menu_items = menuItems.replace("module.exports =", "");
 		this.menu_items = this.menu_items.slice(0, this.menu_items.length-1);				
 		this.menu_items = JSON.parse(this.menu_items);
-		this.menu_items = this.menu_items.menu_items;
-
+	 
+		console.log(this.menu_items);
 		//TODO: sort the menu items by position
 	}
 
