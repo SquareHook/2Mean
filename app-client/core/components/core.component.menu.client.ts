ac61708793bc66ec925aa028b44817b43796d3a4
Hayden Shelton <hayden.shelton@utah.edu>
2017-01-05 12:01:44 -0700

    fixed tabs

--- app-client/core/components/core.component.menu.client.ts
+++ app-client/core/components/core.component.menu.client.ts
@@ -12,27 +12,25 @@ export class CoreMenuComponent{
 	menu: any
 	constructor()
 	{
-		//TODO: filter out items and subitems by role
+	  //TODO: filter out items and subitems by role
+	  let menuItems;
+	  //do some formatting to get proper JSON
+	  menuItems = menuJson.replace("module.exports =", "");
+	  menuItems = menuItems.slice(0, menuItems.length-1);				
+      menuItems = JSON.parse(menuItems);
 
-		let menuItems;
-		//do some formatting to get proper JSON
-		menuItems = menuJson.replace("module.exports =", "");
-		menuItems = menuItems.slice(0, menuItems.length-1);				
-		menuItems = JSON.parse(menuItems);
+      //sort the menu items according to the position variable
+      menuItems.sort((a :any, b :any) =>{
+	    return a.position - b.position;
+	  });
 
-		//sort the menu items according to the position variable
-		menuItems.sort((a :any, b :any) =>{
-			return a.position - b.position;
-		});
-
-		for(let item of menuItems)
-		{
-			if(item.subitems && item.subitems.length > 0)
-			{
-				item.dropdown=true;
-			}
+	  for(let item of menuItems)
+	  {
+		if(item.subitems && item.subitems.length > 0)
+		  {
+			item.dropdown=true;
+		   }
 		}
-		console.log(menuItems);
 		this.menu = menuItems;
 	}
 }	
