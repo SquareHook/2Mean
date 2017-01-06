59bdadee195ef87b563dc9fffc6499ff807e55ab
dylan-wright <dylan.wright@squarehook.com>
2017-01-06 14:03:30 -0700

    SDK-78 config tweak and mixed some style problems

--- modules/app/client/app.component.spec.ts
+++ modules/app/client/app.component.spec.ts
@@ -3,12 +3,16 @@
 import { async, ComponentFixture, TestBed }   from '@angular/core/testing';
 import { By }                                 from '@angular/platform-browser';
 import { DebugElement }                       from '@angular/core';
+import { RouterTestingModule }                from '@angular/router/testing';
 
 /* UUT */
 import { AppComponent }                       from './app.component';
 
 /* Dependencies */
+import { CoreModule }                         from '../../core/client/core.module';
+import { AppRoutingModule }                   from './app-routing.module';
 import { AuthService }                        from '../../auth/client/auth.service.client';
+import { UserService }                        from '../../users/client/services/user.service';
 
 describe('AppComponent', () => {
   let comp:     AppComponent;
@@ -16,8 +20,17 @@ describe('AppComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        AppRoutingModule,
+        CoreModule,
+        RouterTestingModule
+      ],
       declarations: [ 
         AppComponent
+      ],
+      providers: [
+        UserService,
+        AuthService
       ]
     });
   }));
--- modules/users/client/services/user.service.spec.ts
+++ modules/users/client/services/user.service.spec.ts
@@ -1,5 +1,11 @@
 /* Vendor */
-import { BaseRequestOptions, Http, HttpModule, Response, ResponseOptions } from '@angular/http';
+import { 
+  BaseRequestOptions, 
+  Http, 
+  HttpModule, 
+  Response, 
+  ResponseOptions 
+} from '@angular/http';
 import { MockBackend, MockConnection } from '@angular/http/testing';
 import { async, inject, TestBed } from '@angular/core/testing';
 
@@ -58,39 +64,56 @@ describe('UserService', () => {
   });
 
   // Test service can be injected
-  it('should construct', async(inject([ UserService, MockBackend], (userService : UserService, mockBackend : MockBackend) => {
-    expect(userService).toBeDefined();
-  })));
+  it('should construct', async(inject(
+    [ UserService, MockBackend], 
+    (userService : UserService, mockBackend : MockBackend) => {
+      expect(userService).toBeDefined();
+    }
+  )));
 
   // Test CRUD methods
   describe('#create', () => {
-    it('should create a user and get that user back', async(inject([ MockBackend, UserService ], (mockBackend : MockBackend, userService : UserService) => {
-      expect('not').toEqual('implemented');
-    })));
+    it('should create a user and get that user back', async(inject(
+      [ MockBackend, UserService ], 
+      (mockBackend : MockBackend, userService : UserService) => {
+        expect('not').toEqual('implemented');
+      }
+    )));
 
-    it('should not create an invalid user', async(inject([ MockBackend, UserService ], (mockBackend : MockBackend, userService : UserService) => {
-      expect('not').toEqual('implemented');
-    })));
+    it('should not create an invalid user', async(inject(
+      [ MockBackend, UserService ], 
+      (mockBackend : MockBackend, userService : UserService) => {
+        expect('not').toEqual('implemented');
+      }
+    )));
   });
 
   describe('#read', () => {
 
-    it('should get a user', async(inject([ MockBackend, UserService ], (mockBackend : MockBackend, userService : UserService) => {
-      mockBackend.connections.subscribe((conn : MockConnection) => {
-        conn.mockRespond(new Response(new ResponseOptions({ body: { data: JSON.stringify(mockUser) } })));
-      });
+    it('should get a user', async(inject(
+      [ MockBackend, UserService ], 
+      (mockBackend : MockBackend, userService : UserService) => {
+        mockBackend.connections.subscribe((conn : MockConnection) => {
+          conn.mockRespond(new Response(new ResponseOptions(
+            { body: { data: JSON.stringify(mockUser) } }
+          )));
+        });
 
-      userService.read('5866b8c0f5b5613933d30a21').subscribe((user: User) => {
-        expect(user).toEqual(mockUser);
-      });
-    })));
+        userService.read('5866b8c0f5b5613933d30a21').subscribe((user: User) => {
+          expect(user).toEqual(mockUser);
+        });
+      }
+    )));
 
-    it('should fail to get a user that doesnt exist',
-      async(inject([ MockBackend, UserService ], (mockBackend: MockBackend, userService : UserService) => {
+    it('should fail to get a user that doesnt exist', async(inject(
+      [ MockBackend, UserService ], 
+      (mockBackend: MockBackend, userService : UserService) => {
         const correct = "Not sure";
 
         mockBackend.connections.subscribe((conn: MockConnection) => {
-          conn.mockRespond(new Response(new ResponseOptions({ body: { data: 'not sure what this is supposed to be yet. Need to check api' } })));
+          conn.mockRespond(new Response(new ResponseOptions(
+            { body: { data: 'not sure what this is supposed to be yet. Need to check api' } }
+          )));
         });
 
         userService.read('a').subscribe((user: User) => {
@@ -100,22 +123,34 @@ describe('UserService', () => {
   });
 
   describe('#update', () => {
-    it('should update a user and get that user back', async(inject([ MockBackend, UserService ], (mockBackend : MockBackend, userService : UserService) => {
-      expect('not').toEqual('implemented');
-    })));
+    it('should update a user and get that user back', async(inject(
+      [ MockBackend, UserService ], 
+      (mockBackend : MockBackend, userService : UserService) => {
+        expect('not').toEqual('implemented');
+      }
+    )));
 
-    it('should not update a user with invalid info', async(inject([ MockBackend, UserService ], (mockBackend : MockBackend, userService : UserService) => {
-      expect('not').toEqual('implemented');
-    })));
+    it('should not update a user with invalid info', async(inject(
+      [ MockBackend, UserService ], 
+      (mockBackend : MockBackend, userService : UserService) => {
+        expect('not').toEqual('implemented');
+      }
+    )));
   });
 
   describe('#delete', () => {
-    it('should delete a user and get that user back', async(inject([ MockBackend, UserService ], (mockBackend : MockBackend, userService : UserService) => {
-      expect('not').toEqual('implemented');
-    })));
+    it('should delete a user and get that user back', async(inject(
+      [ MockBackend, UserService ], 
+      (mockBackend : MockBackend, userService : UserService) => {
+        expect('not').toEqual('implemented');
+      }
+    )));
 
-    it('should not delete a user which doesnt exist', async(inject([ MockBackend, UserService ], (mockBackend : MockBackend, userService : UserService) => {
-      expect('not').toEqual('implemented');
-    })));
+    it('should not delete a user which doesnt exist', async(inject(
+      [ MockBackend, UserService ], 
+      (mockBackend : MockBackend, userService : UserService) => {
+        expect('not').toEqual('implemented');
+      }
+    )));
   });
 });
