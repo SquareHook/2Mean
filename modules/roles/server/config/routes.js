1db05dee9ea12f444a7dde2c14cd502666ac8382
Hayden Shelton <hayden.shelton@utah.edu>
2017-01-31 11:29:31 -0700

    remembered to resecure the routes

--- modules/roles/server/config/routes.js
+++ modules/roles/server/config/routes.js
@@ -3,30 +3,30 @@ module.exports = [
   route: '/roles',
   type: 'POST',
   method: 'create',
-  secure: false
+  secure: true
 },
 {
   route: '/roles',
   type: 'PUT',
   method: 'update',
-  secure: false
+  secure: true
 },
 {
   route: '/roles/:id',
   type: 'DELETE',
   method: 'delete',
-  secure: false
+  secure: true
 },
 {
   route: '/roles/tree/:id?',
   type: 'GET',
   method: 'tree',
-  secure: false
+  secure: true
 },
 {
   route: '/roles/subroles/:id',
   type: 'GET',
   method: 'subroles',
-  secure: false
+  secure: true
 }
 ];
