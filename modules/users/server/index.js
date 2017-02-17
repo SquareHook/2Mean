86d26141778b6528849ba8760fe5b7447ffae2a0
dylan-wright <dylan.wright@squarehook.com>
2017-02-17 16:06:02 -0700

    SDK-243 started refactoring

--- modules/users/server/index.js
+++ modules/users/server/index.js
@@ -1,14 +1,22 @@
 var Users = require('./models/Users');
 
-var UserController = require('./controllers/user.controller');
+var UserCrudController = require('./controllers/user-crud.controller');
+var UserProfileController = require('./controllers/user-profile.controller');
+var UserAuthController = require('./controllers/user-auth.controller');
 
 /**
  * Top level function that wraps all of the module together to return to the application.
  */
 function User([logger]) {
-  var userController = new UserController(logger);
+  var userCrudController = new UserCrudController(logger);
+  var userProfileController = new UserProfileController(logger);
+  var userAuthController = new UserAuthController(logger);
 
-  return userController;
+  return {
+    crud: userCrudController,
+    profile: userProfileController,
+    auth: userAuthController
+  };
 };
 
 module.exports = User;
