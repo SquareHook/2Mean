ed1915e113f26586986e0d7890720e6604c62260
dylan-wright <dylan.wright@squarehook.com>
2017-04-13 13:47:57 -0600

    SDK-371 change password in form and controller

--- modules/users/server/index.js
+++ modules/users/server/index.js
@@ -8,7 +8,7 @@ var UserAuthController = require('./controllers/user-auth.controller');
  * Top level function that wraps all of the module together to return to the application.
  */
 function User([logger, shared]) {
-  var userCrudController = new UserCrudController(logger);
+  var userCrudController = new UserCrudController(logger, shared);
   var userProfileController = new UserProfileController(logger, shared);
   var userAuthController = new UserAuthController(logger, shared);
 
