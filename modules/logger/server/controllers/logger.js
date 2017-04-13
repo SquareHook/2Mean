37d2bc55cd8eb02af59f12d1c2edecf44c92a4f3
dylan-wright <dylan.wright@squarehook.com>
2017-04-13 09:41:01 -0600

    SDK-344 client logger and relay

--- modules/logger/server/controllers/logger.js
+++ modules/logger/server/controllers/logger.js
@@ -110,6 +110,26 @@ function Logger() {
 
     return meta;
   }
+
+  /**
+   * endpoint controller for relaying client side log messages to server
+   * and possibly elasticsearch
+   * @param {String} req.body.level
+   * @param {Object} req.body.args
+   */
+  function relayLog(req, res, next) {
+    // level must be set. args may be anything but undefined
+    if (req.body.level && req.body.args !== undefined) {
+      logger.log(req.body.level, req.body.args);
+      res.status(200).send();
+    } else {
+      res.status(400).send({ message: 'missing log level' });
+    }
+  }
+
+  return {
+    relayLog: relayLog
+  }
 };
 
 module.exports = new Logger();
