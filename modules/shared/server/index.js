cad4c5e1d7cfd59abacaaf7bce7c9bbd979df739
dylan-wright <dylan.wright@squarehook.com>
2017-04-27 11:38:23 -0600

    SDK-383 SDK-305 SDK-344 upgraded to angular4, upgraded to webpack2, fixed client logger and tested it

--- modules/shared/server/index.js
+++ modules/shared/server/index.js
@@ -1,13 +1,16 @@
 const UploaderController = require('./controllers/uploader.controller');
+const ClientLoggerController = require('./controllers/client-logger.controller');
 
 /**
  * Top level function that wraps all of the module together to return to the application.
  */
 function Shared([logger]) {
   let uploaderController = new UploaderController(logger);
+  let clientLoggerController = new ClientLoggerController(logger);
 
   return {
-    uploader: uploaderController
+    uploader: uploaderController,
+    clientLogger: clientLoggerController
   };
 };
 
--- modules/users/client/components/signin.component.ts
+++ modules/users/client/components/signin.component.ts
@@ -10,6 +10,8 @@ import { User }             from './../models/user.model';
 /*  Angular2 Services */
 import { AuthService }      from './../../../auth/client/services/auth.service';
 
+import { LoggerService } from '../../../shared/client/services/logger.service';
+
 @Component({
   templateUrl: './../views/signin.view.html'
 })
@@ -22,8 +24,10 @@ export class SigninComponent implements OnInit {
     private notificationsService: NotificationsService,
     private authService: AuthService, 
     private router: Router,
-    private activatedRoute: ActivatedRoute
-  ) { }
+    private activatedRoute: ActivatedRoute,
+    private logger: LoggerService
+  ) { 
+  }
 
   ngOnInit() {
   }
@@ -35,6 +39,12 @@ export class SigninComponent implements OnInit {
     this.authService.login(this.model.userName, this.model.password, (error, data) => {
       // server has returned data
       if (data) {
+    this.logger.silly('Silly');
+    this.logger.debug('Debug');
+    this.logger.verbose('Verbose');
+    this.logger.info('Info');
+    this.logger.warn('Warn');
+    this.logger.error('Error');
         // is there a return url
         // honor query param first then authService
         this.activatedRoute.queryParams.subscribe((params) => {
