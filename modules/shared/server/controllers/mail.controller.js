cf4d75502a838bf0650887b575838dfb2c4aad51
dylan-wright <dylan.wright@squarehook.com>
2017-04-28 14:04:39 -0600

    SDK-369 email model

--- modules/shared/server/controllers/mail.controller.js
+++ modules/shared/server/controllers/mail.controller.js
@@ -5,6 +5,8 @@ const config = require('../../../../config/config');
 
 const sesApiVersion = '2010-12-01';
 
+const Email = require('../models/email.model');
+
 function MailController(logger) {
   const knownProviders = [
     'ses'
@@ -21,9 +23,11 @@ function MailController(logger) {
    */
   function sendMail(params) {
     return new Promise((resolve, reject) => {
+      let email = new Email(params.text, params.to, params.from, params.subject);
+
       if (config && config.email && config.email.provider) {
         if (config.email.provider === 'ses') {
-          resolve(sendMailSES(params));
+          resolve(sendMailSES(email));
         } else {
           reject(new Error('Unknown email provider: ' + config.email.provider));
         }
@@ -33,14 +37,10 @@ function MailController(logger) {
 
   /**
    * send and email using aws-ses
-   * @param {Object} params
-   * @param {String} params.text - text of email
-   * @param {String} params.from - email sent from
-   * @param {String} params.to - email sent to
-   * @param {String} params.subject - subject of email
+   * @param {Email} email
    * @returns {Promise}
    */
-  function sendMailSES(params) {
+  function sendMailSES(email) {
     // create a new SES transport
     let transporter = nodemailer.createTransport({
       SES: new aws.SES({
@@ -51,7 +51,7 @@ function MailController(logger) {
     // wrap callback in promise
     return new Promise((resolve, reject) => {
       // use SES transporter to send the message
-      transporter.sendMail(params, (err, info) => {
+      transporter.sendMail(email, (err, info) => {
         if (err) {
           reject(err);
         } else {
