0c783f28d00d108b843df216c72f426304b5208f
dylan-wright <dylan.wright@squarehook.com>
2017-05-01 15:53:14 -0600

    SDK-305 fixed test config

--- config/webpack/webpack.test.js
+++ config/webpack/webpack.test.js
@@ -8,42 +8,45 @@ module.exports = {
   devtool: 'inline-source-map',
 
   resolve: {
-    extensions: [ '', '.ts', '.js' ],
-    moduleDirectories: [ 'node_modules', path.resolve(rootDir, 'modules/app/client') ]
+    extensions: [ '.ts', '.js' ],
+    modules: [ 'node_modules', path.resolve(rootDir, 'modules/app/client') ]
   },
-  tslint: {
-    emitErrors: true
-  },
-
   module: {
-    preLoaders: [
-        { exclude: /node_modules/, loader: 'tslint', test: /\.ts$/ }
-    ],
-    loaders: [
+    rules : [
+      {
+        enforce: 'pre',
+        exclude: /node_modules/,
+        loader: 'tslint-loader',
+        test: /.ts$/,
+        options: {
+          configFile: './config/tslint.json',
+          emitErrors: true
+        }
+      },
       { 
         test: /\.ts$/, 
         loaders: ['awesome-typescript-loader', 'angular2-template-loader']
       },
       { 
         test: /\.(css|html)$/, 
-        loader: 'raw' 
+        loader: 'raw-loader' 
       },
       {
         test: /\.less$/,
-        loader: 'null'
-      }
-    ],
-    postLoaders: [
+        loader: 'null-loader'
+      },
       { 
+        enforce: 'post',
         test: /\.(js|ts)$/, 
-        loader: 'istanbul-instrumenter?esModules=true', 
+        loader: 'istanbul-instrumenter-loader?esModules=true', 
         include: /modules\/.*\/client/,
         exclude: [ /\.(e2e|spec)\.ts/, /node_modules/ ] 
       }
     ]
   },
-  tslint: {
-    configFile: './config/tslint.json'
+  entry: {
+    app: [ path.resolve(rootDir, 'modules/app/client', 'main') ],
+    vendor: [ path.resolve(rootDir, 'modules/app/client', 'vendor') ]
   },
   plugins: [
     new ContextReplacementPlugin(
