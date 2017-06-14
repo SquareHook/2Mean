f610c822712933169332823c9eb7adc916eacf9e
Jason Bennett <jasonb@squarehook.com>
2017-06-14 11:03:31 -0600

    removed redundant code

--- server.js
+++ server.js
@@ -39,12 +39,6 @@ var https_options = {
   cert: fs.readFileSync('./config/private/cacert.pem')
 };
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(cookieParser());
-
 /*
  * Express setup.
  */
