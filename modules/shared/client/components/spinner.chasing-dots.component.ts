dabc025e03e00fe89a800c8a8e59a5a34dab6a86
Kevin Haupt <kshaupty@gmail.com>
2017-09-19 15:35:50 -0600

    Updating 2mean with latest additions to WP-manager and a button fix

---
+++ modules/shared/client/components/spinner.chasing-dots.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'spinner-chasing-dots',
+  styleUrls: [ '../styles/spinner.css' ],
+  templateUrl: '../views/chasing-dots.html'
+})
+export class SpinnerChasingDotsComponent {
+  // Message for accessability
+
+  constructor() {
+
+  }
+}
---
+++ modules/shared/client/components/spinner.double-bounce.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'spinner-double-bounce',
+  styleUrls: [ '../styles/spinner.css' ],
+  templateUrl: '../views/double-bounce.html'
+})
+export class SpinnerDoubleBounceComponent {
+  // Message for accessability
+
+  constructor() {
+
+  }
+}
